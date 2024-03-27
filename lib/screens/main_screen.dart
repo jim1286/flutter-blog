5798668725172ec9fe4bb2f95637a12155170182
jimin <jimin1286@gmail.com>
2024-03-27 16:34:27 +0900

    [Feat] create list view

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_blog/proviers/post.provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -54,58 +55,92 @@ class _MainScreenState extends ConsumerState<MainScreen> {
       body: Column(
         children: [
           const SizedBox(height: 30),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () => _logout(),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.blue,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "로그아웃",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w800),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () => _logout(),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            "로그아웃",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () => _navigateToCreatePostScreen(),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "글 작성",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w800),
+                    GestureDetector(
+                      onTap: () => _navigateToCreatePostScreen(),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            "글 작성",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              postList.when(
-                data: (postList) {
-                  if (postList.isEmpty) {
-                    return const Text('empty');
-                  }
+                  ],
+                ),
+                const SizedBox(height: 50),
+                postList.when(
+                  data: (postList) {
+                    if (postList.isEmpty) {
+                      return const Text('empty');
+                    }
 
-                  return Column(
-                      children: [for (var post in postList) Text(post.title)]);
-                },
-                error: (error, stack) => const Center(child: Text("error")),
-                loading: () => const Center(child: CircularProgressIndicator()),
-              )
-            ],
+                    return SizedBox(
+                      width: 400,
+                      height: 400,
+                      child: ListView.separated(
+                        itemCount: postList.length,
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        itemBuilder: (context, index) {
+                          var post = postList[index];
+
+                          return Container(
+                            width: 250,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Center(
+                              child: Text(post.title),
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const SizedBox(
+                            width: 40,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  error: (error, stack) => const Center(child: Text("error")),
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                )
+              ],
+            ),
           ),
         ],
       ),
